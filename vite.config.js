fca88d9e7f41fb3352eba3a9481ababd6b2cf014
choizia <st.jyeon@gmail.com>
2025-11-19 22:20:18 +0900

    change path

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig(({ mode }) => {
           target: env.VITE_API_BASE,
           changeOrigin: true,
           secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''), // '/api/foo' -> '/foo'
           configure: (proxy, options) => {
             proxy.on('proxyReq', (proxyReq, req) => {
               console.log('[proxyReq]', req.method, req.url, '=>', options.target)
